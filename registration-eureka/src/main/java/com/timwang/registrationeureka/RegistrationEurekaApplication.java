1f6bb74f58f9aae817af6eb5c2ea2c6f7dfbb014
timwang <kevintimwang@gmail.com>
2020-04-06 02:57:11 +0800

    增加注册eureka服务配置spring-boot

---
+++ registration-eureka/src/main/java/com/timwang/registrationeureka/RegistrationEurekaApplication.java
@@ -0,0 +1,25 @@
+package com.timwang.registrationeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/*
+* 我们需要在主Spring Boot应用程序类文件中添加@EnableDiscoveryClient批注。
+* @EnableDiscoveryClient注释使您的Spring Boot应用程序充当Eureka客户端。
+* */
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class RegistrationEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationEurekaApplication.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String home() {
+        return "Eureka Client application";
+    }
+}
---
+++ registration-eureka/src/test/java/com/timwang/registrationeureka/RegistrationEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timwang.registrationeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
