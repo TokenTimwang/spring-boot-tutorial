dadd1034f833a7690824783370a6a77dbb8451ea
timwang <kevintimwang@gmail.com>
2020-04-06 03:07:09 +0800

    增加描述eureka配置spring-boot

--- registration-eureka/src/main/java/com/timwang/registrationeureka/RegistrationEurekaApplication.java
+++ registration-eureka/src/main/java/com/timwang/registrationeureka/RegistrationEurekaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 /*
 * 我们需要在主Spring Boot应用程序类文件中添加@EnableDiscoveryClient批注。
 * @EnableDiscoveryClient注释使您的Spring Boot应用程序充当Eureka客户端。
+* https://juejin.im/post/5d78cd53f265da03d55e8351
 * */
 @SpringBootApplication
 @EnableDiscoveryClient
